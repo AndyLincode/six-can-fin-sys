90589134f0037811687891eeaf0ebe907b43d4e7
Andy Lin <ld130102@gmail.com>
2023-06-21 17:35:03 +0800

    FIX: add btn+input & style adjust 後續加入datepiker

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,66 @@ export default function Home() {
     <main className="h-[100vh]">
       <div className="top-container bg-[#274B8A] h-[35%] border-b-2 border-white flex justify-center items-center relative">
         <svg viewBox="0 0 100 100" width="100%" height="auto">
-          <path id="MyPath" fill="none" d="m-78,65c60,-30 200,-30 250,0" />
-          <path id="MyEngPath" fill="none"  d="m-78,75c100,-30 200,-30 250,0" />
-          <text fill="#AFC8E8" fontWeight="bold" fontSize="22px" textLength='258%'>
+          <path id="MyPath" fill="none" d="m-78,60c55,-32 195,-30 250,0" />
+          <path id="MyEngPath" fill="none" d="m-78,70c95,-32 195,-30 250,0" />
+          <text
+            fill="#AFC8E8"
+            fontWeight="bold"
+            fontSize="22px"
+            textLength="258%"
+          >
             <textPath href="#MyPath">自動理財管理系統</textPath>
           </text>
-          <text fill="#AFC8E8" fontWeight="bold" fontSize="6px" textLength='252%'>
+          <text
+            fill="#AFC8E8"
+            fontWeight="bold"
+            fontSize="6px"
+            textLength="252%"
+          >
             <textPath href="#MyEngPath">
               Automatic Financial Management System
             </textPath>
           </text>
         </svg>
-        <p className="sub-title text-white text-2xl absolute bottom-12"><span className="font-black">·</span> 六罐子理財法 <span className="font-black">·</span></p>
+        <p className="sub-title text-white text-2xl absolute bottom-[30%]">
+          <span className="font-black">·</span> 六罐子理財法{" "}
+          <span className="font-black">·</span>
+        </p>
       </div>
       <div className="bottom-container bg-[#4874B4] h-[65%] flex justify-center relative">
-        <div className="form-wrap bg-[#AFC8E8] w-[30%] h-fit py-5 flex items-center flex-col absolute top-[-6%] rounded-2xl">
-          <button className=" outline outline-3 outline-white rounded-xl w-[70%] my-3 bg-[#AFC8E8] font-bold text-white">收入</button>
-          <button className=" outline outline-3 outline-white rounded-xl w-[70%] my-3 bg-[#AFC8E8] font-bold text-white">支出</button>
+        <div className="form-wrap bg-[#AFC8E8] w-[30%] h-fit pt-6 pb-8 flex items-center flex-col absolute top-[-10%] rounded-2xl border-4 border-white">
+          <button className=" outline outline-3 outline-white rounded-xl w-[70%] my-6 bg-[#AFC8E8] font-bold text-white">
+            收入
+          </button>
+          <button className=" outline outline-3 outline-white rounded-xl w-[70%] my-6 bg-[#AFC8E8] font-bold text-white">
+            支出
+          </button>
           <div className="fee-item w-full flex justify-center">
-            <select name="item" id="item" className="outline outline-3 outline-white rounded-xl w-[32%] my-3 bg-[#AFC8E8] font-bold text-white mr-8 px-2">
-              <option value="-" selected>項目</option>
+            <select
+              name="item"
+              id="item"
+              className="outline outline-3 outline-white rounded-xl w-[32%] my-6 bg-[#AFC8E8] font-bold text-white mr-8 px-2"
+            >
+              <option value="-" selected>
+                項目
+              </option>
             </select>
-            <input type="text" className="outline outline-3 outline-white rounded-xl w-[32%] my-3 bg-[#AFC8E8] font-bold text-white" />
+            <input
+              type="text"
+              className="outline outline-3 outline-white rounded-xl w-[32%] my-6 bg-[#AFC8E8] font-bold text-white text-center placeholder:text-white focus:placeholder:text-[#ffffff44] focus:outline-2"
+              placeholder="備註"
+            />
           </div>
-          
+          {/* TODO 做datepiker */}
+          <input
+            type="date"
+            name=""
+            id=""
+            className=" outline outline-3 outline-white rounded-xl w-[70%] my-6 bg-[#AFC8E8] font-bold text-white px-5"
+          />
+          <button className=" outline outline-3 outline-white rounded-2xl my-2 px-5 py-1 bg-[#AFC8E8] font-bold text-white hover:bg-white hover:text-[#4874B4]">
+            確定
+          </button>
         </div>
       </div>
     </main>
